39c7ec78dd32d1cdd830315812131162360f50fb
Gyeoul <clsmay.1st@gmail.com>
2024-10-04 23:14:04 +0900

    [미션2] 깃허브 리포지토리에 프로젝트 올리기

---
+++ src/main/kotlin/dev/gyeoul/threadev/KotlinThreadevApplication.kt
@@ -0,0 +1,11 @@
+package dev.gyeoul.threadev
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinThreadevApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinThreadevApplication>(*args)
+}
---
+++ src/test/kotlin/dev/gyeoul/threadev/KotlinThreadevApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.gyeoul.threadev
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinThreadevApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
